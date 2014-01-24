0bca781fe3f6e7879b097700947fdea05cc42dea
senatwj <senatwj@rose-hulman.edu>
2014-01-23 21:44:30 -0500

    working scrollable background - not perfect

---
+++ shooter/Assets/Scripts/BGScroller.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BGScroller : MonoBehaviour {
+
+	public float speed = 2;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	void FixedUpdate () {
+
+		if (Camera.main.WorldToScreenPoint (transform.position).y < 0 - (Screen.height / 2)) {
+			transform.position = new Vector3 (0, 10 - (speed * Time.deltaTime), 1);
+		} else {
+			Vector3 newPos = transform.position;
+			newPos.y -= speed * Time.deltaTime;
+			transform.position = newPos;
+		}
+
+	}
+}

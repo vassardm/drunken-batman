5ee9866dd3698bba02523c354be3186fbf423db2
Omar Abdel <omar.abdelrahman@gmail.com>
2014-02-04 10:53:20 -0500

    added medium enemies, waves, invulnerability after death

---
+++ shooter/Assets/Scripts/Enemy/MedEnemyAI.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MedEnemyAI : MonoBehaviour {
+
+	public GameObject enemyBullet;
+	public float shootSpeed;
+	public float time;
+	public float startShootTime;
+	public GameObject oneUp;
+	public GameObject extraBomb;
+	public GameObject fireRate;
+	public GameObject points;
+	public int health;
+	
+	public GameBehavior gameScript;
+	
+	// Use this for initialization
+	void Start () {
+		time = Time.time + shootSpeed + startShootTime;
+		health = 3;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.X))
+		{
+			//Creates an object, giving it position, and 
+			Instantiate(enemyBullet,transform.position,transform.rotation);
+		}
+		
+		if (Time.time > time) {
+			Instantiate(enemyBullet, transform.position, transform.rotation);
+			time += shootSpeed;
+		}
+		
+	}
+	
+	void OnTriggerEnter2D(Collider2D other){
+		if(other.tag.Equals("PlayerBullet")){
+			health--;
+			BulletAI bulletAI = other.GetComponent<BulletAI>();
+			bulletAI.bulletDestroy();
+			
+		}
+		else if(other.tag.Equals("Player")){
+			health--;
+		}
+		if(health < 1){
+			gameScript.scoreCounter++;
+			print ("score = " + gameScript.scoreCounter);
+			var randNumb = Random.Range(1, 100);
+			if(randNumb <= 5){
+				Instantiate(points, transform.position, transform.rotation);
+				print ("extra life");
+			}
+			else if(randNumb >= 90){
+				Instantiate(extraBomb, transform.position, transform.rotation);
+				print ("extra bomb");
+			}
+			else if(randNumb >= 80){
+				Instantiate(fireRate, transform.position, transform.rotation);
+				print ("fire rate");
+			}
+			else if(randNumb >= 70){
+				Instantiate(points, transform.position, transform.rotation);
+				print ("more points");
+			}
+			Destroy (gameObject);
+		}
+	}
+}
+
---
+++ shooter/Assets/Scripts/manyEnemies.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class manyEnemies : MonoBehaviour {
+	
+	public GameObject basicEnemy;
+	public GameObject medEnemy;
+	//public GameObject hardEnemy;
+	public float spawnAreaWidth;
+	public float spawnAreaHeight;
+	public int numOfEnemiesX;
+	public int numOfEnemiesY;
+	public float secondWaveTime;
+	private bool secondReady = true;
+	private bool thirdReady = true;
+	public float thirdWaveTime;
+	private float startTime;
+	private float totalTime;
+	
+	// Use this for initialization
+	void Start () {
+		startTime = Time.time;
+		for(int i = 0; i < numOfEnemiesX; i++){
+			for(int j = 0; j < numOfEnemiesY; j++){
+				Vector2 spawnPosition = transform.position;
+				spawnPosition.x += i * (spawnAreaWidth/numOfEnemiesX);
+				spawnPosition.y -= j * (spawnAreaHeight/numOfEnemiesY);
+				Instantiate(basicEnemy,spawnPosition, basicEnemy.transform.rotation);
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		totalTime = Time.time - startTime;
+		if(totalTime > secondWaveTime && secondReady){
+			spawnSecond();
+		}
+		if(totalTime > thirdWaveTime && thirdReady){
+			spawnThird();
+		}
+		
+	}
+	
+	void spawnSecond () {
+		return;
+	}
+	void spawnThird () {
+		return;
+	}
+}

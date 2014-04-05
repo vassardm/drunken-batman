0683fadf202a5a71ffec256dc23f8895092010bb
Kyle Bippus <kbipp49@fastmail.fm>
2014-04-05 15:02:38 -0400

    Changed key-detecting logic so that only one enemy spawns per button press.

--- shooter/Assets/Scripts/SandboxSpawn.cs
+++ shooter/Assets/Scripts/SandboxSpawn.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 /// <summary>
 /// Controls the enemy spawn behavior of the sanbox scene
@@ -8,20 +9,21 @@ public class SandboxSpawn : MonoBehaviour {
 
 	public GameObject enemy1, enemy2, enemy3, enemy4;
 
-	void Update() {
-		if (Input.GetKey (KeyCode.Alpha1)) 
+	void Update() 
+    {
+		if (Input.GetKeyDown(KeyCode.Alpha1)) 
         {
             SpawnEnemy(enemy1);
         }
-        else if (Input.GetKey(KeyCode.Alpha2))
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
         {
             SpawnEnemy(enemy2);
         }
-        else if (Input.GetKey(KeyCode.Alpha3))
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
         {
             SpawnEnemy(enemy3);
         }
-        else if (Input.GetKey(KeyCode.Alpha4))
+        else if (Input.GetKeyDown(KeyCode.Alpha4))
         {
             SpawnEnemy(enemy4);
         }
@@ -29,16 +31,7 @@ public class SandboxSpawn : MonoBehaviour {
 
     private void SpawnEnemy(GameObject enemy)
     {
-        foreach (GameObject existingEnemy in GameObject.FindGameObjectsWithTag("Enemy"))
-        {
-            if (existingEnemy.Equals(enemy))
-            {
-                print("Name: " + existingEnemy.name);
-                return;
-            }
-        }
-
-        Vector3 spawnPoint = new Vector3(-1, 1, 0);
-        Instantiate(enemy, spawnPoint, enemy.transform.rotation);
+            Vector3 spawnPoint = new Vector3(-1.5f, 2, 0);
+            Instantiate(enemy, spawnPoint, enemy.transform.rotation);
     }
 }

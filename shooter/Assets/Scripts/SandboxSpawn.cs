49370c0305bfcb11632a815d85675a1f6a7cf213
Kyle Bippus <kbipp49@fastmail.fm>
2014-04-05 12:10:44 -0400

    Can spawn boss, but need to restrict copies of enemies for now.

--- shooter/Assets/Scripts/SandboxSpawn.cs
+++ shooter/Assets/Scripts/SandboxSpawn.cs
@@ -11,19 +11,33 @@ public class SandboxSpawn : MonoBehaviour {
 	void Update() {
 		if (Input.GetKey (KeyCode.Alpha1)) 
         {
-            Instantiate(enemy1);
+            SpawnEnemy(enemy1);
         }
         else if (Input.GetKey(KeyCode.Alpha2))
         {
-            Instantiate(enemy2);
+            SpawnEnemy(enemy2);
         }
         else if (Input.GetKey(KeyCode.Alpha3))
         {
-            Instantiate(enemy3);
+            SpawnEnemy(enemy3);
         }
         else if (Input.GetKey(KeyCode.Alpha4))
         {
-            Instantiate(enemy4);
+            SpawnEnemy(enemy4);
         }
 	}
+
+    private void SpawnEnemy(GameObject enemy)
+    {
+        foreach (GameObject existingEnemy in GameObject.FindGameObjectsWithTag("Enemy"))
+        {
+            if (existingEnemy.name.Equals(enemy.name))
+            {
+                return;
+            }
+        }
+
+        Vector3 spawnPoint = new Vector3(-1, 1, 0);
+        Instantiate(enemy, spawnPoint, enemy.transform.rotation);
+    }
 }

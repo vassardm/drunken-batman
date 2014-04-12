263aa7ce47193489a2c4e5c14c1be9f4fa8a5341
Kyle Bippus <kbipp49@fastmail.fm>
2014-04-12 00:27:33 -0400

    Downloaded iTween Path, started interfaces

---
+++ shooter/Assets/Scripts/Enemy/New/IEnemyMovement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class IEnemyMovement
+{
+    float Speed { get; set; }
+
+    Vector3[] GetRelativePath(string pathName, Vector3 relativePoint)
+    {
+        Vector3[] path = iTweenPath.GetPath(pathName);
+        Vector3[] result = new Vector3[path.Length];
+
+        for (int i = 0; i < path.Length; i++)
+        {
+            result[i] = relativePoint + path[i];
+        }
+
+        return result;
+    }
+}

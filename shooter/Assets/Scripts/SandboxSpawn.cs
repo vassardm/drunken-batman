02c6e80cdf4ee8421ffabe22e9535e1a0fc05a7d
Kyle Bippus <kbipp49@gmail.com>
2014-05-03 16:25:12 -0400

    Waves complete

--- shooter/Assets/Scripts/SandboxSpawn.cs
+++ shooter/Assets/Scripts/SandboxSpawn.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 /// <summary>
 /// Controls the enemy spawn behavior of the sanbox scene
@@ -7,28 +8,31 @@ using System.Collections;
 public class SandboxSpawn : MonoBehaviour 
 {
 	public GameObject enemy1, enemy2, enemy3, enemy4, enemy5;
+    public string pathName1, pathName2, pathName3, pathName4, pathName5;
     public GameBehavior gameScript;
 
 	void Update() {
+        Vector3 defaultSpawnPoint = new Vector3(0, 0, 0);
+
 		if (Input.GetKeyDown(KeyCode.Alpha1)) 
         {
-            SpawnEnemy(enemy1);
+            SpawnEnemy(enemy1, iTweenPath.GetPath(pathName1)[0]);
         }
         else if (Input.GetKeyDown(KeyCode.Alpha2))
         {
-            SpawnEnemy(enemy2);
+            SpawnEnemy(enemy2, defaultSpawnPoint);
         }
         else if (Input.GetKeyDown(KeyCode.Alpha3))
         {
-            SpawnEnemy(enemy3);
+            SpawnEnemy(enemy3, defaultSpawnPoint);
         }
         else if (Input.GetKeyDown(KeyCode.Alpha4))
         {
-            SpawnEnemy(enemy4);
+            SpawnEnemy(enemy4, defaultSpawnPoint);
         }
         else if (Input.GetKeyDown(KeyCode.Alpha5))
         {
-            SpawnEnemy(enemy5);
+            SpawnEnemy(enemy5, defaultSpawnPoint);
         }
 
         if (Input.GetKeyDown(KeyCode.J))
@@ -37,7 +41,7 @@ public class SandboxSpawn : MonoBehaviour
         }
 	}
 
-    private void SpawnEnemy(GameObject enemy)
+    private void SpawnEnemy(GameObject enemy, Vector3 spawnPoint)
     {
         foreach (GameObject existingEnemy in GameObject.FindGameObjectsWithTag("Enemy"))
         {
@@ -48,7 +52,6 @@ public class SandboxSpawn : MonoBehaviour
             }
         }
 
-        Vector3 spawnPoint = iTweenPath.GetPath("BasicPath4")[0];
         Instantiate(enemy, spawnPoint, enemy.transform.rotation);
     }
 }

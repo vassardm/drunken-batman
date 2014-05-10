c721195310ce1f2ff2c00141999ba7d4d67def82
Kyle Bippus <kbipp49@gmail.com>
2014-05-09 21:58:35 -0400

    Enemies can fire simple bullets now.

---
+++ shooter/Assets/Scripts/Enemy/New/SimpleBulletPattern.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Handles simple bullet-creation logic
+/// </summary>
+public class SimpleBulletPattern : MonoBehaviour {
+    public GameObject bullet;
+    public float timeBetweenShots;
+    public float startShootTime;
+
+    private float time;
+
+    void Start()
+    {
+        time = Time.time + timeBetweenShots + startShootTime;
+    }
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (Time.time > time)
+        {
+            Instantiate(bullet, transform.position, transform.rotation);
+            time += timeBetweenShots;
+        }
+	}
+}

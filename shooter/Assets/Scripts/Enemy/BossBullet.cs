b6f9a053a0775a09591979ebed43bb37546aad67
Kyle Bippus <kbipp49@gmail.com>
2014-05-14 14:15:55 -0400

    Forgot to commit boss bullet stuff for TH

--- shooter/Assets/Scripts/Enemy/BossBullet.cs
+++ shooter/Assets/Scripts/Enemy/BossBullet.cs
@@ -4,18 +4,22 @@ using System.Collections;
 public class BossBullet : MonoBehaviour {
 
 	public GameBehavior globals;
-	public float velocity = -6.5f;
+	public float velocity;
 	public bool grazeTrigger = false;
 	public bool entered = false; // You need this flag or the game will try to read EVERY instance of the collison!
+
+    private THorseSystemMechanics th;
 	
 	// Use this for initialization
 	void Start () {
+
+        th = Camera.main.GetComponent<THorseSystemMechanics>();
 		
 		Vector2 newVelocity = Vector2.zero;
 		float rotation = gameObject.transform.eulerAngles.z;
 		//print ("rotation" + rotation);
-		newVelocity.y = -Mathf.Cos (rotation * Mathf.Deg2Rad) * velocity;
-		newVelocity.x = Mathf.Sin (rotation * Mathf.Deg2Rad) * velocity;
+		newVelocity.y = -Mathf.Cos (rotation * Mathf.Deg2Rad) * (velocity + th.GetModifier(3));
+		newVelocity.x = Mathf.Sin (rotation * Mathf.Deg2Rad) * (velocity + th.GetModifier(3));
 		rigidbody2D.velocity = newVelocity;
 		globals = Camera.main.GetComponent<GameBehavior>();
 		
